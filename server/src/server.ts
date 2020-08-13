3048301a48c4c1bf28a8e9595ea825e79fff2877
Jonathan Kim <jnthnk@outlook.com>
2020-08-14 00:26:10 +0200

    Test some requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.log('Testo')
+import express from 'express'
+
+// Create Express App
+const app = express()
+
+// Enable JSON requests
+app.use(express.json())
+
+//
+app.get('/users', (req, res) => {})
+
+//
+app.post('/user', (req, res) => {
+  req.body
+
+  return res.json(false)
+})
+
+app.listen(3333)
