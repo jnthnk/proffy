c1ce6dc78cded26b770e4568d724cdb73db0c521
Jonathan Kim <jnthnk@outlook.com>
2020-08-14 17:14:27 +0200

    Add cors to express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 
 // Create Express App
@@ -6,6 +7,7 @@ const app = express()
 
 // Use JSON and routes
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 // Listen to port 3333
